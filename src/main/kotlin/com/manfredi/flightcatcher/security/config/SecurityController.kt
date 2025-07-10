a6a0099fb79edbb7f861c24250edca1a57d28a4b
FranManfredi <85314753+FranManfredi@users.noreply.github.com>
2025-07-09 23:52:50 -0300

    deleted unused import

--- src/main/kotlin/com/manfredi/flightcatcher/security/config/SecurityController.kt
+++ src/main/kotlin/com/manfredi/flightcatcher/security/config/SecurityController.kt
@@ -1,7 +1,5 @@
 package com.manfredi.flightcatcher.security.config
 
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
-
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.Customizer
